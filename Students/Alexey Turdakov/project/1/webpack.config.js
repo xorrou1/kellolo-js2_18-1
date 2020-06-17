76be23356c71b832afbccde73087de2ab3ef52b3
Alexey Turdakov <49039563+AlexeyTurdakov@users.noreply.github.com>
2020-06-17 13:20:39 +0300

    add homeWork_1 (задания HUman, Hamburger)

---
+++ Students/Alexey Turdakov/project/1/webpack.config.js
@@ -0,0 +1,24 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    
+    plugins: [new MiniCssExtractPlugin({
+            filename: 'style/[name].css',
+            chunkFilename: '[id].css',
+        }),
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ],
+    module: {
+        rules: [{
+            test: /\.css$/i,
+            use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        }, ],
+    },
+    devServer: {
+        port: 3000,
+        open: false,
+    }
+};
