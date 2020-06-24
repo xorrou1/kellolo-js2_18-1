06562862452a5d04a456d0e978b46615d1d78a4c
evgeniya-koltsova <66247709+evgeniya-koltsova@users.noreply.github.com>
2020-06-24 15:44:21 +0300

    Add files via upload

--- Students/Evgeniya Koltsova/project/src/components/main.js
+++ Students/Evgeniya Koltsova/project/src/components/main.js
@@ -1,4 +1,4 @@
-let names = ['HTML5 & CSS3', 'JavaScript base', 'JavaScript advanced', 'PHP', 'React'];
+/* let names = ['HTML5 & CSS3', 'JavaScript base', 'JavaScript advanced', 'PHP', 'React'];
 let prices = [100, 120, 130, 50, 150];
 let ids = [1, 2, 3, 4, 5];
 let imgs = ['http://placehold.it/200x150', 'http://placehold.it/200x150', 'http://placehold.it/200x150', 'http://placehold.it/200x150', 'http://placehold.it/200x150',]
@@ -11,12 +11,162 @@ let createItem = index => ({
 });
 
 let fillCatalog = () => {
-    ids.forEach((el, index) => {
-        catalog.items.push(createItem(index));
-    });
-};
+    //ids.forEach((el, index) => {
+    //    catalog.items.push(createItem(index));
+    //});
 
-let basket = {
+    return ids.map((el, index) => createItem(index))
+}; */
+
+class List {
+    constructor(url, container, basket) {
+        this.url = 'https://raw.githubusercontent.com/evgeniya-koltsova/online-store-api/master/responses' + url;
+        this.container = container;
+        this.items = [];
+        this._init(basket);
+    }
+
+    _init(basket = false) {
+        this._get(this.url)
+            .then(data => {
+                this.items = !basket ? data : data.contents;
+                this._render();
+                this._handleEvents();
+            })
+    }
+
+    _get(url) {
+        return fetch(url).then(data => data.json())
+    }
+
+    _render() {
+        let htmlStr = '';
+        this.items.forEach (item => {
+            htmlStr += new connect[this.constructor.name](item).render();
+        })
+        document.querySelector(this.container).innerHTML = htmlStr;
+    }
+
+    _handleEvents() {
+        return '';
+    }
+}
+
+class Item {
+    constructor(item){
+        this.item = item;
+    }
+
+    render() {
+        return `<div class="catalog-item">
+                        <img src="${this.item.img}" alt="${this.item.product_name}">
+                        <div class="desc">
+                            <h3>${this.item.product_name}</h3>
+                            <p>${this.item.price} $</p>
+                            <button 
+                                class="buy-btn" 
+                                name="buy"
+                                data-name = "${this.item.product_name}"
+                                data-price = "${this.item.price}"
+                                data-id="${this.item.id_product}"
+                            >Buy</button>
+                        </div>
+                    </div>`
+    }
+}
+
+class Catalog extends List {
+    constructor(basket, url = '/catalogData.json', container = '.catalog-items') {
+        super(url, container);
+        this.basket = basket;
+    }
+
+    _handleEvents() {
+        document.querySelector(this.container).addEventListener('click', evt => {
+            if(evt.target.name == 'buy') {
+                this.basket.add(evt.target.dataset)
+            }
+        })
+    }
+}
+
+class Basket extends List {
+    constructor(url = '/getBasket.json', container = '.basket-items', basket = true) {
+        super(url, container, basket);
+    }
+
+    _handleEvents() {
+        document.querySelector(this.container).addEventListener('click', evt => {
+            if(evt.target.name == 'remove') {
+                this.remove(evt.target.dataset.id);
+            }
+        })
+
+        document.querySelector('.btn-basket').addEventListener('click', evt => {
+            document.querySelector('.basket-block').classList.toggle('invisible');
+        });
+    }
+
+    add(item) {
+        let find = this.items.find(el => el.id_product == item.id);
+        if(find) {
+            find.quantity++;
+            this._render();
+        } else {
+            let itemNew = {id_product: item.id, product_name: item.name, price: +item.price, quantity: 1};
+            this.items.push(itemNew);
+            this._render();
+        }
+    }
+
+    remove(itemId) {
+        let find = this.items.find(el => el.id_product == itemId);
+        if (find.quantity == 1) {
+            this.items.splice(this.items.indexOf(find), 1);
+        } else {
+            find.quantity--;
+        }
+        this._render();
+    }
+}
+
+class CatalogItem extends Item {
+
+}
+
+class BasketItem extends Item {
+    constructor(item) {
+        super(item)
+    }
+
+    render() {
+        return `<div class="basket-item">
+                        <img src="http://placehold.it/100x80" alt="${this.item.product_name}">
+                        <div class="product-desc">
+                            <p class="product-title">${this.item.product_name}</p>
+                            <p class="product-amount">${this.item.quantity}</p>
+                            <p class="product-single-price">${this.item.price}</p>
+                        </div>
+                        <div class="right-block">
+                            <p class="product-price">${this.item.price * this.item.quantity}</p>
+                            <button class="del-btn" name="remove" data-id="${this.item.id_product}">&times;</button>
+                        </div>
+                    </div>`
+    }
+}
+
+
+let connect = {
+    'Catalog': CatalogItem,
+    'Basket': BasketItem
+}
+
+export default () => {
+    let basket = new Basket();
+    let catalog = new Catalog(basket); 
+}
+
+/* let basket = {
     items: [],
     show: false,
     container: '.basket-items',
@@ -117,3 +267,4 @@ export default () => {
     basket.init();
     catalog.init();   
 }
+*/
