ffe4b4698b3e95cdcdabd28e4e42fc366412ee88
yudindn <yudindn@gmail.com>
2020-07-01 12:10:36 +0300

    Перезалил проект из лекции №6

---
+++ Students/Dmitry Yudin/PROJECT/src/components/Basket.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="basket-block">
+        <div class="basket-items">
+            <item itemType="basket"/>            
+        </div>
+    </div>
+</template>
+
+<script>
+import item from './Item.vue'
+export default {
+    components: { item },
+    data() {
+        return {
+            items: [],
+            url: 'https://raw.githubusercontent.com/gavrilovem/catalogData/master/getBasket.json'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
