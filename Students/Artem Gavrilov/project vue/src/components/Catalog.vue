f80c54f95c389e61e85f3b1a7bb302095ead8f50
Artem <Gavrilov>
2020-07-02 17:53:21 +0500

    added Shop.vue + components

---
+++ Students/Artem Gavrilov/project vue/src/components/Catalog.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="catalog-items">
+        <!--Catalog elems-->
+        <item 
+            itemType="catalog"
+            v-for="item of items"
+            :item="item"
+            :key="item.id_product"
+        />
+    </div>
+    
+</template>
+
+<script>
+import item from './Item.vue'
+export default {
+    components: { item },
+    data() {
+        return {
+            url: 'https://raw.githubusercontent.com/gavrilovem/catalogData/master/catalogData.json',
+            items: []
+        }
+    },
+    mounted() {
+        this.$parent.get(this.url)
+            .then( d => this.items = d )
+    }
+}
+</script>
+
+<style>
+
+</style>
--- Students/Artem Gavrilov/project vue/src/index.js
+++ Students/Artem Gavrilov/project vue/src/index.js
@@ -1,6 +1,12 @@
-import app from './components/main.js'
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './layout/css/style.css'
 import './layout/css/normalize.css'
 
+import Vue from 'vue';
+import app from './views/Shop.vue';
 
-app();
+new Vue({
+    render: h => h(app)
+}).$mount('#app')
