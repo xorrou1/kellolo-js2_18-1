dbfe07a9d711d401f9df9eebca7008d10b8a1941
Andrey <Andreisham123@gmail.com>
2020-07-03 14:49:03 +0300

    домашка к 6 уроку

---
+++ Students/Andrey Shamarin/project/src/components/Item.vue
@@ -0,0 +1,53 @@
+<template>
+	<div>
+		<template v-if="itemType == 'catalog'">
+			<div class="catalog-item">
+                <img src="http://placehold.it/300x200" alt="item.product_name">
+                <div class="desc">
+                    <h3>{{item.product_name}}</h3>
+                    <p>{{item.price}} $</p>
+                    <button 
+                        class="buy-btn" 
+                        name="buy"
+                        @click="$parent.$parent.$refs.basketComp.add(item)"
+                    >Buy</button>
+                </div>
+            </div>
+		</template>
+
+		<template v-else-if="itemType == 'basket'">
+			<div class="basket-item">
+				<img src="http://placehold.it/100x80" alt="item.product_name"> 
+				<div class="basket-items">
+					<div class="product-desc">
+						<p class="product-title">{{item.product_name}}</p>
+						<p class="product-amount">{{item.quantity}}</p>
+						<p class="product-single-price">{{item.price}}</p>
+					</div>
+					<div class="right-block">
+						<p class="product-price">{{item.price * item.quantity}}</p>
+						<button class="del-btn" name="remove" @click="$parent.$parent.refs.basketComp.remove(item)">&times;</button>
+					</div>
+				</div>
+			</div>
+		</template>
+	</div>	
+</template>
+
+<script>
+export default {
+	props: {
+		itemType: {
+			type: String
+		},
+		item: {
+			type: Object
+		}
+	}
+}
+//this.$parent.$parent.refs.basketComp.add(item) - обращение к методу от "дяди"
+</script>
+
+<style>
+
+</style>
---
+++ Students/Andrey Shamarin/project/views/Shop.vue
@@ -0,0 +1,54 @@
+<template> 
+    <div>
+		<header>
+            <div class="logo">GeekShop</div>
+            <div class="basket">
+                <form action="#" class="search-form">
+                    <input type="text" class="search-field">
+                    <button class="btn-search">
+                        <i class="fas fa-search"></i>
+                    </button>
+                </form>
+                <button class="btn-basket" @click="showBasket = !showBasket">Корзина</button> <!--при клике на кноку, свойство баскет поменяется на противоположное-->
+                <basket ref="basketComp" />
+            </div>
+        </header>
+        <main>
+			<catalog />
+        </main>
+    </div>
+</template>
+
+<script>
+import Catalog from '../src/components/Catalog.vue'
+import Basket from '../src/components/Basket.vue'
+
+export default {
+	
+	components: {
+		// блок для регистрации дочерних компонентов
+		'catalog': Catalog,
+		'basket': Basket
+	},
+	data() {
+		return {
+			showBasket: false
+		}
+	},
+	props: {
+		// пробрасывааемые внутри данные, которые не изменяются внутри компонента
+	},
+	methods: {
+		_get(url) {
+            return fetch(url).then(d => d.json());
+		},
+	},
+	mounted() {
+		// this.$refs.basketComp // обращение к дочернему компоненту Basket
+	}
+}
+</script>
+
+<style>
+
+</style>
